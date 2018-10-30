3020329a2d0dcc1f553e7648324334c55a8b5543
Sarah Pickrahn <spickrahn@gmail.com>
2018-10-30 11:18:40 -0700

    worked on get request stuff

---
+++ hrsf105-front-end-capstone/client/src/components/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const List = () => {
+  // in this component i map over the list passed down through props
+  // and pass each comment's info into the comment component
+};
+
+export default List;
--- hrsf105-front-end-capstone/server/server.js
+++ hrsf105-front-end-capstone/server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.static('client/dist'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/comments', (req, res) => {
+  console.log('response from server side get request: ', res);
+  res.status(200).send(JSON.stringify(res));
+});
 
 app.listen(port, () => {
   console.log('listening..............o.o');
