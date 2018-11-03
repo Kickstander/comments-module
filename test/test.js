6d23e24012b6a969496f9414c8357e93f3915029
Sarah Pickrahn <spickrahn@gmail.com>
2018-11-02 19:37:16 -0700

    imported components to test file

--- test/test.js
+++ test/test.js
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import sampleData from './sampledata.js';
+import List from '../src/components/List';
+import App from '../src/components/App';
+import Comment from '../src/components/Comment';
+import Post from '../src/components/Post';
+import Replies from '../src/components/Replies';
+import Reply from '../src/components/Reply';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,10 +20,13 @@ test('fake test should be true', () => {
   expect(2 + 2).toBe(4);
 });
 // test that comments render to the page
-test('correct number of comments render to page', () => {
-  const wrapper = shallow(<List list={sampleData} />);
-  expect(wrapper.find(<Comment />)).toBe(4);
+describe('List component', () => {
+  test('correct number of comments render to page', () => {
+    const wrapper = shallow(<List list={sampleData} />);
+    expect(wrapper.find(<Comment />)).toBe(4);
+  });
 });
+
 
 // test that comments show author name
 
