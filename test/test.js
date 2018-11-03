14782e6d0bfa89982ae54b8d9c0304568f61dfcb
Sarah Pickrahn <spickrahn@gmail.com>
2018-11-02 19:00:31 -0700

    add one unit test for List

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sampleData from 'sampleData.js';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // test('replies is array', () => {
 //   expect(Array.isArray(dataGenFunctions.generateReplies())).toBe(true);
@@ -8,7 +14,10 @@ test('fake test should be true', () => {
   expect(2 + 2).toBe(4);
 });
 // test that comments render to the page
-// test('');
+test('correct number of comments render to page', () => {
+  const wrapper = shallow(<List list={sampleData} />);
+  expect(wrapper.find(<Comment />)).toBe(4);
+});
 
 // test that comments show author name
 
