a601cc01739494d1512dc12647e515e3c7248894
Sarah Pickrahn <spickrahn@gmail.com>
2018-11-02 20:04:19 -0700

    change stuff

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
-import { configure } from 'enzyme';
+import { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import sampleData from './sampledata.js';
 import List from '../client/src/components/List';
@@ -13,16 +12,18 @@ import Reply from '../client/src/components/Reply';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// test('replies is array', () => {
-//   expect(Array.isArray(dataGenFunctions.generateReplies())).toBe(true);
-// });
+test('fakedata is array', () => {
+  expect(Array.isArray(sampleData)).toBe(true);
+});
+
 test('fake test should be true', () => {
   expect(2 + 2).toBe(4);
 });
+
 // test that comments render to the page
 describe('List component', () => {
   test('correct number of comments render to page', () => {
-    const wrapper = shallow(<List list={sampleData} />);
+    const wrapper = mount(<List list={sampleData} />);
     expect(wrapper.find(<Comment />)).toBe(4);
   });
 });
