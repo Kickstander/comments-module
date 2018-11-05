9880b32af6e61edf19ff2342b2a6e2e1ad8102f2
Sarah Pickrahn <spickrahn@gmail.com>
2018-11-05 15:46:03 -0800

    add cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const databaseExports = require('../database/index.js');
 
@@ -6,10 +8,9 @@ const app = express();
 
 const port = 3001;
 
-app.use(express.static('client/dist'));
-
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
 app.get('/comments', (req, res) => {
